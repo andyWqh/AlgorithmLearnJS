dcf4d2768b768d87cfac511605d80664f5b6e172
andyWqh <andywqh@gmail.com>
2018-11-07 00:20:09 +0800

    Add SelectOrder Files

--- OrderAlgorithrmFiles/SelectSort.js
+++ OrderAlgorithrmFiles/SelectSort.js
@@ -1,4 +1,4 @@
-//冒泡排序算法
+//选择排序算法
 function ArrayList() {
     //存储数据数组
     var array = [];
@@ -12,23 +12,11 @@ function ArrayList() {
         return array.join(",");
     };
 
-    //冒泡排序
-    this.bubleSort = function () {
+    //选择排序
+    this.selectSort = function () {
         var length = array.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    this.swap(j, j + 1);
-                }
-            }
-        }
-    };
-
-    //改进冒泡排序
-    this.modifyBubleSort = function () {
-        var length = array.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
                     this.swap(j, j + 1);
                 }
@@ -58,16 +46,14 @@ function createNodeSort(size) {
     return array;
 }
 
-//测试冒泡排序
+//测试选择排序
 $(function () {
     var array = createNodeSort();
-    console.log("测试冒泡排序=》排序前原数组:");
+    console.log("测试选择排序=》排序前原数组:");
     console.log(array.print());
     //排序
-    //array.bubleSort();
-    //改进冒泡排序
-    array.modifyBubleSort();
+    array.selectSort();
     //排序后的值
-    console.log("测试冒泡排序=》排序后数组:");
+    console.log("测试选择排序=》排序后数组:");
     console.log(array.print());
 });
